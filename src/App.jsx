cb3940a0341b055176cee9e0a301e1a07dccbfb0
Jamelle Messinger <jamswoof914@gmail.com>
2025-02-06 14:40:42 -0700

    add subreddit component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import NavBar from './components/NavBar';
+import Subreddits from './components/Subreddits';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Subreddits />
     </>
   );
 }
---
+++ src/components/Subreddits.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Subreddits() {
+  return (
+    <div className="subreddits">
+      <h2>Subreddits</h2>
+      <ul>
+        <li>Subreddit 1</li>
+        <li>Subreddit 2</li>
+        <li>Subreddit 3</li>
+      </ul>
+    </div>
+  );
+}
