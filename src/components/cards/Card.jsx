cc0fa9415e398d8b5858ed70f8603eb10b9c2761
Jamelle Messinger <jamswoof914@gmail.com>
2025-02-11 10:55:14 -0700

    update styling

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -13,7 +13,39 @@ export default function Card({ postData }) {
   return (
     <div className={css.card}>
       <div className={css.karma}>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m4.5 15.75 7.5-7.5 7.5 7.5"
+            />
+          </svg>
+        </button>
         <p>{score}</p>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
       </div>
       <div className={css.main}>
         <div className={css.userContent}>
