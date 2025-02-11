6ac8426113c8e5affbd6db56d2e962f5cb340a14
Jamelle Messinger <jamswoof914@gmail.com>
2025-02-10 21:06:41 -0700

    add search icon to search bar button

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -3,14 +3,31 @@ import css from '../../css/NavBar.module.css';
 
 export default function NavBar() {
   return (
-    <div className={css.navbar} id='top'>
+    <div className={css.navbar} id="top">
       <div className={css.logo}>
         <img src="/logo.png" alt="logo" />
-        <h1><span>Reddit</span>Minimal</h1>
+        <h1>
+          <span>Reddit</span>Minimal
+        </h1>
       </div>
       <div className={css.search}>
         <input type="text" placeholder="Search" />
-        <button>Search logo here</button>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
